c4f5561ddb022d3c960cbcf7273edad216723ee4
Mingcong Bai <jeffbai@aosc.io>
2024-04-09 20:22:23 +0800

    fix(sunloginclient-fixup.sh): fix Git merge error

--- static/scripts/sunloginclient-fixup.sh
+++ static/scripts/sunloginclient-fixup.sh
@@ -10,9 +10,5 @@ cp -v /usr/local/sunlogin/scripts/runsunloginclient.service \
     /etc/systemd/system/runsunloginclient.service
 systemctl enable runsunloginclient.service --now
 
-<<<<<<< HEAD
-echo "正在修正皮肤权限位 ..."
-=======
 echo "正在修正皮肤数据包权限位 ..."
->>>>>>> 0eadd18 (feat(scripts): add fix up script for sunloginclient)
 chmod 666 /usr/local/sunlogin/res/skin/*
